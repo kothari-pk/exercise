6b707bb3be7dd3876b0532e357f7080d0d36bf57
kothari-pk <monu.prateek@gmail.com>
2017-11-26 12:48:53 -0800

    Another trie with Autocomplete

--- src/main/scala/datastructure/TrieNode.scala
+++ src/main/scala/datastructure/TrieNode.scala
@@ -34,3 +34,39 @@ class TrieNode {
       else children(index).startsWith(s.substring(1))
   }
 }
+
+class TrieNode2(val prefix: String) {
+  val children: scala.collection.mutable.Map[Char, TrieNode2] = scala.collection.mutable.Map.empty[Char, TrieNode2]
+  var isEnd: Boolean = false
+
+  def add(word: String): Unit = word.headOption match {
+    case None => isEnd = true
+    case Some(l) => children.getOrElseUpdate(l, new TrieNode2(s"$prefix$l")).add(word.substring(1))
+  }
+
+  def isWord(s: String): Boolean = s.headOption match {
+    case None => isEnd
+    case Some(l) => children.get(l) match {
+      case None => false
+      case Some(t) => t.isWord(s.substring(1))
+    }
+  }
+
+  def getWordsForPrefix(p: String): List[String] = {
+    if (p == prefix) findAllChildrenWords(this)
+    else {
+      children.values.toList.find(_.prefix == p) match {
+        case Some(t) => findAllChildrenWords(t)
+        case None => List.empty[String]
+      }
+    }
+  }
+
+  def findAllChildrenWords(n: TrieNode2, result: List[String] = List.empty[String]): List[String] =
+  if (n.children.isEmpty) result :+ n.prefix
+  else {
+    val r = if (n.isEnd) result :+ n.prefix else result
+    n.children.values.toList.flatMap(x => findAllChildrenWords(x, r))
+  }
+
+}
